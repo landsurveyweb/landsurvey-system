8b868351f8315359bb4786c4679e3c752627c3af
landsurveyweb <cslagithub@gmail.com>
2026-08-10 12:02:21 +0800

    Load latest map script version

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -45,7 +45,7 @@ installApiAdapter(supabase, session.user);
 
 await new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./js/map.js";
+    script.src = "./js/map.js?v=202608101200";
     script.onload = resolve;
     script.onerror = reject;
     document.body.appendChild(script);
